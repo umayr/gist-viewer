7c779ee37da83f91f81dfb996369cc305614f113
Umayr Shahid <umayr.shahid@gmail.com>
2018-03-20 02:31:14 +0500

    Add service worker

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
+import registerServiceWorker from './worker';
+
 import store from './store';
 import App from './module/App';
 
 import './style.css';
+
+registerServiceWorker();
 
 const target = document.querySelector('#root');
 
